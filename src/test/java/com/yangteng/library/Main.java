6296f175eab26eb162da62833a29e81125c64379
林河 <2832294398@qq.com>
2023-01-15 15:43:26 +0800

    更新

---
+++ src/test/java/com/yangteng/library/Main.java
@@ -0,0 +1,9 @@
+package com.yangteng.library;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(Test4.class);
+    }
+}
--- src/test/java/com/yangteng/library/Test4.java
+++ src/test/java/com/yangteng/library/Test4.java
@@ -1,13 +1,29 @@
 package com.yangteng.library;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Test4 extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         var dialog = new Dialog<>();
+        var dialogPane = new DialogPane();
+        dialog.setDialogPane(dialogPane);
+        var box = new VBox();
+        var button = new Button("打开");
+        var box1 = new VBox();
+        box1.getChildren().add(new Label("打不开"));
+        dialogPane.setContent(box1);
+        button.setOnMouseClicked(e -> dialog.show());
+        box.getChildren().add(button);
+        var scene = new Scene(box);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
